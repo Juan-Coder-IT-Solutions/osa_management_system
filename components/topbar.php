68b85c8c990841daf80cd705fccd91ab7c12d47f
John Mark <johnmark.foroffice@gmail.com>
2023-09-21 09:02:11 +0800

    update
    -fix profile

--- components/topbar.php
+++ components/topbar.php
@@ -1,3 +1,8 @@
+  <?php
+    $user_id = $_SESSION['user_id'];
+    $getUser = $mysqli->query("SELECT * FROM tbl_users WHERE user_id ='$user_id' ");
+    $row = $getUser->fetch_array();
+  ?>
   <header id="header" class="header fixed-top d-flex align-items-center">
 
     <div class="d-flex align-items-center justify-content-between">
@@ -27,7 +32,7 @@
         <li class="nav-item dropdown pe-3">
 
           <a class="nav-link nav-profile d-flex align-items-center pe-0" href="#" data-bs-toggle="dropdown">
-            <!-- <img src="assets/img/profile-img.jpg" alt="Profile" class="rounded-circle"> -->
+          <img src="assets/upload/<?=$row['profile_img']?>" alt="Profile" class="rounded-circle">
             <span class="d-none d-md-block dropdown-toggle ps-2"><?=user_info("user_fname",$user_id)." ".user_info("user_lname",$user_id)?></span>
           </a><!-- End Profile Iamge Icon -->
 
